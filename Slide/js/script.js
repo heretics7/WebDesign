5d335687230ab13e8fda4c738f7eda09eee79561
heretics7 <heretics7@naver.com>
2022-03-15 11:48:15 +0900

    헤더, 슬라이드 정리

---
+++ Slide/js/script.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+  /* slider-left */
+  
+  var widthMv = $("#slider_left").width();
+  var dir = -1;
+
+  setInterval(function(){
+    
+    $("#slider_left ul").animate( { "marginLeft": widthMv*dir}, 400, function(){
+      $(this).find("li").eq(0).appendTo($(this));
+      $(this).css("margin-left", 0);
+    });
+
+  },3000)
+
+  /* slider-top */
+  
+  var heightMv = $("#slider_top").height();
+
+  setInterval(function(){
+    
+    $("#slider_top ul").animate( { "marginTop": heightMv*dir}, 400, function(){
+      $(this).find("li").eq(0).appendTo($(this));
+      $(this).css("margin-top", 0);
+    });
+
+  },3000)
+
+  /* Fade */
+  var classNm = "show";
+  var count = 0;
+  var total = $("#slider_fade li").length;
+
+  setInterval(function(){
+    count++;
+    count %= total;
+
+    $("#slider_fade li").eq(count).addClass(classNm).siblings().removeClass(classNm)
+    
+
+  },3000)
+
+
+})
